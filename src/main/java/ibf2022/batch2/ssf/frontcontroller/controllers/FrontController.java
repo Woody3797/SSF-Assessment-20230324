d9b978663df6c15a0cda0f3b7684ce96aa561630
Woody3797 <woody3797@gmail.com>
2023-03-24 09:04:16 +0800

    first commit

---
+++ src/main/java/ibf2022/batch2/ssf/frontcontroller/controllers/FrontController.java
@@ -0,0 +1,13 @@
+package ibf2022.batch2.ssf.frontcontroller.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class FrontController {
+
+	// TODO: Task 2, Task 3, Task 4, Task 6
+	@PostMapping
+	public String 
+	
+}
